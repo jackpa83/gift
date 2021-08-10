92046671b454b5f3032225d611dbdc81c4e4036e
jackpa83 <jackson180183@gmail.com>
2021-08-10 12:31:19 -0400

    animacion

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/animacion.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title @yield('title') >Inifinity Gift</title>
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,41 @@
 
 <x-header/>
 <x-navbar/>
+<br><br><br><br><br><br>
+<div class="container-fluid">
+<x-animacion/>
+</div>
+ <section class="bg-dark">
+     <div class="jumbotron jumbotron-fluid">
+         <div class="container">
+             <h1 class="display-3">Fluid jumbo heading</h1>
+             <p class="lead">Jumbo helper text</p>
+             <hr class="my-2">
+             <p>More info</p>
+             <p class="lead">
+                 <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+             </p>
+         </div>
+         <div class="container bg-dark">
+            <h1 class="display-3">Fluid jumbo heading</h1>
+            <p class="lead">Jumbo helper text</p>
+            <hr class="my-2">
+            <p>More info</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+            </p>
+        </div>
+        <div class="container">
+             <h1 class="display-3">Fluid jumbo heading</h1>
+             <p class="lead">Jumbo helper text</p>
+             <hr class="my-2">
+             <p>More info</p>
+             <p class="lead">
+                 <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+             </p>
+         </div>
+         
+     </div>
+ </section>
+
 <x-footer/>
